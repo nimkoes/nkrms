3e380f225738c6db70c5b32e58a6efaadbd44e97
nimkoes <xxxelppa@gmail.com>
2021-06-10 14:01:25 +0900

    Initial commit

---
+++ src/test/java/me/nimkoes/nkrms/NkrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nimkoes.nkrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
